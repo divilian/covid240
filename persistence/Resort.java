33e2a502959a7cccf5d3c399ac4223e9ab5c56b8
Stephen <stephen@umw.edu>
2020-06-09 16:41:57 -0400

    Begin hydration code for Resort.

--- persistence/Resort.java
+++ persistence/Resort.java
@@ -1,6 +1,7 @@
 
 import java.io.PrintWriter;
 import java.io.File;
+import java.util.Scanner;
 
 class Resort {
     String name;
@@ -8,6 +9,15 @@ class Resort {
     private String desc;
     private int rating;
     private int price;
+
+    Resort(Scanner s) {
+        this.name = s.nextLine();
+        this.phone = s.nextLine();
+        String starsLine = s.nextLine();   // "3 stars"
+        this.rating = Integer.valueOf(starsLine.split(" ")[0]);
+        String dollarsLine = s.nextLine();  // "$$$$"
+        this.price = dollarsLine.length();
+    }
 
     Resort(String name, String phone, String desc, int rating, int price) {
         this.name = name;
@@ -38,6 +48,7 @@ class Resort {
     }
 
     public static void main(String args[]) {
+/*
         Resort anything = new Resort("Brian's Place", "1111111",
             "this place sucks!!!\nI mean seriously??\nThere's no running water!", 1, 5);
 
@@ -51,6 +62,17 @@ class Resort {
         }
 
         System.out.println(anything);
+*/
+
+        try {
+            File f = new File("bathroomBreak.txt");
+            Scanner s = new Scanner(f);
+            Resort hydratedResort = new Resort(s);
+            System.out.println(hydratedResort);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
     }
 }
 
