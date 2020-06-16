e993dcb7bc3193eb03f530f3a50e3b50b5697f1b
Stephen <stephen@umw.edu>
2020-06-16 16:26:30 -0400

    Add TommyGold's broken constructor example.

---
+++ inheritanceConstructors/JazzMusician.java
@@ -0,0 +1,14 @@
+
+
+
+public class JazzMusician extends TommyGold {
+    private int gold;
+    JazzMusician(int x) {
+        int z = 1 + 1;
+        super();
+        gold = x;
+    }
+    public static void main(String args[]) {
+        
+    }
+}
---
+++ inheritanceConstructors/TommyGold.java
@@ -0,0 +1,10 @@
+
+
+public class TommyGold {
+    TommyGold() {
+        System.out.println("hi!");
+    }
+    public static void main(String args[]) {
+        TommyGold t = new JazzMusician(35);
+    }
+}
