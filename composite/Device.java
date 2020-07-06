93deee21d3168abbf417aeaa0c4db93481e91114
Stephen <stephen@umw.edu>
2020-07-06 18:10:09 +0000

    Add some JavaDoc.

--- composite/Device.java
+++ composite/Device.java
@@ -1,14 +1,38 @@
 
+/**
+ * A <tt>Device</tt> is an entry in a directory which does <b><i>not</i></b>
+ * actually represent a file. Instead it is a mount point for an external
+ * device to provide I/O to the operating system.
+ *
+ * @author SD
+ */
 class Device extends DirEntry {
     String devCode;
     boolean blockDevice;
+
     Device(String devCode, boolean blockDevice) {
         this.devCode = devCode;
         this.blockDevice = blockDevice;
     }
+
+    /**
+     * Always returns false for a device. This is because devices inherently
+     * have no filename. They have no filename because:
+     * <ul>
+     * <li>Filenames suck.</li>
+     * <li>Nobody likes filenames.</li>
+     * </ul>
+     */
     public boolean matches(String s) {
         return false;
     }
+
+    /**
+     * This method does the following three things. First, it .... Second,
+     * it.....
+     * ^^^ that's bad style, because the first sentence doesn't stand well on
+     * its own.
+     */
     public int size() {
         return 0;
     }
--- composite/DirEntry.java
+++ composite/DirEntry.java
@@ -1,5 +1,10 @@
 
 abstract class DirEntry {
+
+    /**
+     * Returns true if any part of this file's name contains the String
+     * passed.
+     */
     abstract boolean matches(String s);
     abstract int size();
 }
